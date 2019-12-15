4566f52163a5b296a93fc7f7457f08e8552694f1
Tiur Mawaddah <tiurzm@gmail.com>
2019-12-15 12:16:20 -0600

    add some code on css file

--- db/schema.sql
+++ db/schema.sql
@@ -10,3 +10,17 @@ CREATE TABLE burgers (
     devoured BOOLEAN DEFAULT false, 
     PRIMARY KEY (id)
 );
+
+-- DROP DATABASE IF EXISTS zk4ieh6yvkd3dr04;
+
+-- CREATE DATABASE zk4ieh6yvkd3dr04;
+-- USE zk4ieh6yvkd3dr04; 
+-- CREATE TABLE burgers (
+--     id INT AUTO_INCREMENT,
+--     burger_name VARCHAR(100),
+--     devoured BOOLEAN DEFAULT false, 
+--     PRIMARY KEY (id)
+-- );
+
+-- USE zk4ieh6yvkd3dr04; 
+-- SELECT * FROM burgers;
--- public/assets/js/function.js
+++ public/assets/js/function.js
@@ -25,23 +25,30 @@ $(function() {
     $(".create-form").on("submit", function(event) {
       // Make sure to preventDefault on a submit event.
       event.preventDefault();
-  
-      var newBurger = {
-        burger_name: $("#nm").val().trim()
-      };
-  
-      // Send the POST request.
-      $.ajax("/api/burgers", {
-        type: "POST",
-        data: newBurger
-      }).then(
-        function() {
-          console.log(newBurger);
-          console.log("created new Burger");
-          // Reload the page to get the updated list
-          location.reload();
-        }
-      );
+
+      let newBurgerName = $("#nm").val().trim();
+      
+      if(newBurgerName) {
+        var newBurger = {
+          burger_name: newBurgerName
+        };
+    
+        // Send the POST request.
+        $.ajax("/api/burgers", {
+          type: "POST",
+          data: newBurger
+        }).then(
+          function() {
+            console.log(newBurger);
+            console.log("created new Burger");
+            // Reload the page to get the updated list
+            location.reload();
+          }
+        );
+      } else {
+        alert("Insert your burger");
+      }
+        
     });
 });
   
